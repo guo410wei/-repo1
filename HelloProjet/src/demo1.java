0af031e32e006a488c80e40156cb114b92d5d15e
admin <zhangsan@itcast.cn>
2018-12-28 18:57:10 +0800

    asdfa

--- HelloProjet/src/demo1.java
+++ HelloProjet/src/demo1.java
@@ -11,4 +11,8 @@ public class demo1 {
 	
 	public void test2() {
         System.out.println("test2......................");
+		}
+	public void test4() {
+        System.out.println("test1......................");
+    }
 }
