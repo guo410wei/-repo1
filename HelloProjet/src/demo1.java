46dd22a8e65f386140fc86b5a70fdaf3cd490179
admin <zhangsan@itcast.cn>
2018-12-28 18:11:40 +0800

    创建了一个demo1的java文件

---
+++ HelloProjet/src/demo1.java
@@ -0,0 +1,11 @@
+public class demo1 {
+
+    
+    public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+    }
+	
+	public void test1() {
+        System.out.println("test1");
+    }
+}
